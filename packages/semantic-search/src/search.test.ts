e8460783a8d6253fde04d09e418c86132c1325df
David Vílchez <dvilchez@xuaps.com>
2023-11-14 19:36:13 +0100

    Rollback search code

--- packages/semantic-search/src/search.test.ts
+++ packages/semantic-search/src/search.test.ts
@@ -1,9 +1,5 @@
-import { search } from "./search";
-
 describe("semantic search", () => {
   it("should return a list of documents", async () => {
-    const docs = await search("test");
-    console.log(docs);
-    expect(docs).toHaveLength(1);
+    expect(true).toBe(true);
   });
 });
--- packages/semantic-search/src/search.ts
+++ packages/semantic-search/src/search.ts
@@ -1,14 +1,3 @@
-import { AutoTokenizer } from "@xenova/transformers";
-
 export async function search(content: string) {
-  const tokenizer = await AutoTokenizer.from_pretrained(
-    "sentence-transformers/all-mpnet-base-v2"
-  );
-  // const model = await AutoModel.from_pretrained(
-  //   "sentence-transformers/all-mpnet-base-v2"
-  // );
-
-  const inputs_ids = tokenizer(content);
-  console.log(inputs_ids);
-  return inputs_ids;
+  return {};
 }
