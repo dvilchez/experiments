c9f6a705c40916b0ee9b30132a82211686843bc4
David Vílchez <dvilchez@xuaps.com>
2023-11-14 21:40:04 +0100

    Add segmentation

--- packages/semantic-search/src/search.test.ts
+++
@@ -1,8 +0,0 @@
-describe("semantic search", () => {
-  it("should return a list of documents related with the query text", async () => {
-    //const querTextEmbeddings = await embed(content)
-    //vextorDb.search(querTextEmbeddings)
-    //[{title: "title", content: "content", score: 0.9}}]
-    expect(true).toBe(true);
-  });
-});
--- packages/semantic-search/src/search.ts
+++
@@ -1,3 +0,0 @@
-export async function search(content: string) {
-  return {};
-}
