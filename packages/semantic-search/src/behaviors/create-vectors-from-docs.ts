99044ebe0035314372b1946c3335738a157afeb7
David Vílchez <dvilchez@xuaps.com>
2023-11-26 20:55:42 +0100

    Give some structure to the code

---
+++ packages/semantic-search/src/behaviors/create-vectors-from-docs.ts
@@ -0,0 +1,15 @@
+import { segmentByWords } from "../model/segmentation";
+import { embedCollection } from "../model/embeddings";
+
+export async function createVectorsFromFiles(files: File[]) {
+  const chunks = await files.reduce(
+    async (acc: Promise<string[]>, file: File) => {
+      const chunks = await acc;
+      const text = await file.text();
+
+      return [...chunks, ...segmentByWords(text, 100)];
+    },
+    Promise.resolve([])
+  );
+  return await embedCollection(chunks);
+}
---
+++ packages/semantic-search/src/index.ts
@@ -0,0 +1,19 @@
+import { Main } from "./ui/main-component";
+import { createVectorsFromFiles } from "./behaviors/create-vectors-from-docs";
+import { searchSimilarDocs } from "./behaviors/search-similar-documents";
+import { Embedding } from "./model/embeddings";
+
+const data: { embeddings: Embedding[]; processedFiles: string[] } = {
+  embeddings: [],
+  processedFiles: []
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const main: Main = document.querySelector("semantic-search");
+  main.onFilesDropped = async (files: File[]) => {
+    data.embeddings = await createVectorsFromFiles(Array.from(files));
+  };
+  main.onSearch = (query: string) => searchSimilarDocs(query, data.embeddings);
+});
+
+window.customElements.define("semantic-search", Main);
