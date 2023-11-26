c7e5ff9bd866bee6798a4054815dd07de911ac74
dvilchez <dvilchez@users.noreply.github.com>
2023-11-26 19:41:10 +0000

    deploy: abc8a02790bc85f40f728892b66c870a71f7ade7

--- semantic-search/src/embeddings.d.ts
+++ semantic-search/src/embeddings.d.ts
@@ -2,9 +2,7 @@ export type Embedding = {
     text: string;
     vector: Float32Array;
 };
-export declare function embed(chunk: string): Promise<Embedding>;
-export declare function embedCollection(chunk: string[]): Promise<Embedding[]>;
-export declare function calculateSimilarities(queryEmbeddings: Embedding, chunksEmbeddings: Embedding[]): {
+export declare const embed: (chunk: string) => Promise<Embedding>, embedCollection: (chunk: string[]) => Promise<Embedding[]>, calculateSimilarities: (queryEmbeddings: Embedding, chunksEmbeddings: Embedding[]) => {
     text: string;
     score: number;
 }[];
